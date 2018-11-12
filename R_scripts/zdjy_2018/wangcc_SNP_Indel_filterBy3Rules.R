0f5fde1e48917518f0cc3774a0442b42ed7d633f
Jimmy <jimmymall@live.com>
2018-11-12 10:37:23 +0800

    v0.0.16-R ZDJY: filter SNP Indel v0.2 correct;

--- R_scripts/zdjy_2018/wangcc_SNP_Indel_filterBy3Rules.R
+++ R_scripts/zdjy_2018/wangcc_SNP_Indel_filterBy3Rules.R
@@ -1,19 +1,21 @@
 ###########
-# v0.1
+# v0.2
 # snp filter script
 #1.by Func; 2. by p value, 3. by GeneName
+#v0.2 correct filter2 by adding as.character();
 ##########
 
 
 #settings
-setwd("D:\\zz_Exon") # set working directory
+setwd("D://zzu_Exon") # set working directory
 fname="test01.csv" #set file name
 project_name="xxx_snp_" #set output file prefix name: xxx_snp_
 
 
+
 #read data
 snp=read.csv(fname,na.strings = "NA") #用npp转为utf-8无bom头编码
-dim(snp) #[1] 738  55
+dim(snp) #[1] 174077     55
 
 head(snp[,1:5]) #
 
@@ -21,7 +23,7 @@ head(snp[,1:5]) #
 #filter 1: by Func.
 # levels(snp$Func) #check values in Func
 snp=snp[snp$Func %in% c("exonic","exonic;splicing","splicing"),]
-dim(snp) #[1] 738  55
+dim(snp) #[1] 22264    55
 
 
 
@@ -33,34 +35,41 @@ pCols=c("X1000g2015aug_Chinese", "X1000g2015aug_eas","X1000g2015aug_all", "esp65
 badRows=c()
 for(i in 1:nrow(snp)){
   for(j in pCols){
-    v=snp[i,j]
+    v=as.character(snp[i,j])
     if(v==".") next
-    if(as.numeric(v)>0.01)
-      #print(i)
+    if(as.numeric(v)>0.01){
       badRows <- c(badRows,i)
+    }
   }
 }
 length(unique(badRows))
 snp2=snp[-unique(badRows),]
 
 #check
-dim(snp2) #[1] 66 55
-snp2[,25:28]
+dim(snp2) #[1] 672  55
+snp2[1:10,25:30]
 
+#output to files
+write.csv(snp2,paste0(project_name,'beforeFilterByGeneList.csv'),row.names = F)
 
 
 
 #filter 3: retain rows which GeneName in list1 and list2 only
-geneList1=c("FOXD2","KIAA0040",'GFI1','C2orf81','IGSF3')
-geneList2=c("TRAK1",'BMP2K','DSPP','IRF2','FDFT1')
+HCM <- readLines("HCM.txt", encoding = "UTF-8")
+#HCM.txt is a txt file with one gene a line, like this
+#MYBPC3
+#MYH7
+#TNNT2
+#TNNI3
+snp_sub1=snp2[snp2$GeneName %in% HCM,]
+dim(snp_sub1) #[1]  5 55
 
-snp_sub1=snp2[snp2$GeneName %in% geneList1,]
-dim(snp_sub1) #[1]  9 55
-
-snp_sub2=snp2[snp2$GeneName %in% geneList2,]
+BPCM <- readLines("BPCM.txt", encoding = "UTF-8")
+snp_sub2=snp2[snp2$GeneName %in% BPCM,]
 dim(snp_sub2) #[1] 10 55
 
 #output to files
-write.csv(snp_sub1,paste0(project_name,'snp_sub1.csv'),row.names = F)
-write.csv(snp_sub2,paste0(project_name,'snp_sub2.csv'),row.names = F)
+write.csv(snp_sub1,paste0(project_name,'HCM.csv'),row.names = F)
+write.csv(snp_sub2,paste0(project_name,'BPCM.csv'),row.names = F)
 
+#end
