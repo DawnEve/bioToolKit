b7e3fb83a480ef03e7eb81e8665b1c5872f8d2e4
jimmy <JimmyMall@live.com>
2022-06-01 19:30:05 +0800

    v0.1.8@Louvain Louvain_4.py 修订版 v2.0, pbmc 3k 数据集可用，虽然不好；但是 10 点小数据集不行;

--- Python/Louvain/Louvain_4.py
+++ Python/Louvain/Louvain_4.py
@@ -1,8 +1,12 @@
 # Aim: 测试 10x 数据: 输入SNN稀疏矩阵，输出2列，和 Seurat 结果作比较。 每个cell一类，失败。
+#  修订版 v2.0, pbmc 3k 数据集可用，虽然不好；但是 10 点小数据集不行;
 import collections
 import random
 import time
 
+
+# Settings
+MAX_PHASE_I=3
 
 # 载入文件: 前两列是数字（顶点编号），中间用空格隔开。
 # 第三列可选，是权重，默认是1。
@@ -15,11 +19,12 @@ def load_graph(path):
             v_j = int(vertices[1])
             w = 1.0
             # 数据集有权重的话则读取数据集中的权重
-            #if len(vertices) > 2:
-            #    w = float(vertices[2])
+            if len(vertices) > 2:
+                w = float(vertices[2])
             # 一行是 1 2 则读入时 2个方向都读入了，分别是 1:{2:1} 和 2:{1:1} 
             G[v_i][v_j] = w
             G[v_j][v_i] = w
+    print(">> total edges:", len(G))
     return G
 
 
@@ -33,6 +38,7 @@ class Vertex:
         self._cid = cid
         self._nodes = nodes
         self._kin = k_in  # 结点内部的边的权重
+    # 方便打印顶点类
     def __str__(self):
         return f"class Vertex:[vid:{self._vid},cid:{self._cid}, nodes:{self._nodes}, kin:{self._kin}]"
     __repr__ = __str__
@@ -52,9 +58,12 @@ class Louvain:
             self._vid_vertex[vid] = Vertex(vid, vid, {vid})
             # 计算边数  每两个点维护一条边
             # 和该点连接的顶点中，比该序号大的点的边数。为了保证只计算一次。
-            self._m += sum([1 for neighbor in self._G[vid].keys() if neighbor > vid])
+            # 这里是不是应该考虑 权重？
+            #self._m += sum([1 for neighbor in self._G[vid].keys() if neighbor > vid])
+            self._m += sum([value for neighbor,value in self._G[vid].items() if neighbor > vid])
+        print("====> m:", self._m)
 
-    # 模块度优化阶段
+    # phaseI 模块度优化阶段
     def first_stage(self):
         mod_inc = False  # 用于判断算法是否可终止
 
@@ -64,52 +73,85 @@ class Louvain:
 
         # 先list才能随机化
         visit_sequence = list(self._G.keys()) #所有顶点
-        # random.shuffle(visit_sequence) # 随机访问 ========================> //todo 为了调试方便，先不打乱了。
+        #random.shuffle(visit_sequence) # 随机访问 ========================> //todo 为了调试方便，先不打乱了。
         #print(">>> visit_sequence in PhaseI:", visit_sequence)
         #print("G:", self._G)
-        n_times=0; #phageI 迭代次数
+        iter_times_phaseI=0; #phageI 迭代次数
 
         while True:
-            n_times+=1;
-            print(f"0=> phaseI iter:{n_times} (max:10)" )
-            if n_times>10: #如果大型网络不收敛，则超过规定次数自动停止
-                break;
+            # 监视最大 deltaQ 值
+            log_max_deltaQ=0
+
+            iter_times_phaseI+=1;
+            print(f"0=> phaseI iter:{iter_times_phaseI} (max:{MAX_PHASE_I})" )
 
             can_stop = True  # 第一阶段是否可终止
-            # 遍历所有节点
+
+            
+            # 遍历所有节点v
             for v_vid in visit_sequence:
+                
+
                 # 获得节点的社区编号
                 v_cid = self._vid_vertex[v_vid]._cid
                 # k_v节点的权重(度数): 外部边权重之和 + 内部边权重(第一轮没有内部边，为0)
                 k_v = sum(self._G[v_vid].values()) + self._vid_vertex[v_vid]._kin
+                
                 # 存储模块度增益大于0的社区编号
                 cid_Q = {}
-                # 遍历节点的邻居节点
+                # 遍历节点的邻居节点w
                 for w_vid in self._G[v_vid].keys():
                     # 获得该邻居的社区编号
                     w_cid = self._vid_vertex[w_vid]._cid
+                    # 如果 v 顶点已经加入过 w 所在的社区，则跳过
                     if w_cid in cid_Q:
                         continue
                     else:
+                        # 计算 deltaQ = deltaQ(D->i) + deltaQ(i->C)
                         # tot是关联到社区C中的节点的链路上的权重的总和
                         # k 是 该邻居节点(w_vid)所在社区(w_cid)的内部点的编号
                         #   k 点 外部权重和 + k点内部权重
                         tot = sum(
-                            [ sum(self._G[k].values()) + self._vid_vertex[k]._kin  for k in self._cid_vertices[w_cid]])
+                            [ sum(self._G[k].values()) + self._vid_vertex[k]._kin  for k in self._cid_vertices[w_cid]] )
                         # 如果该邻居的 社区编号 w_cid 和 顶点的社区编号 v_cid 一致，减去 该顶点的权重
                         # 不是应该跳过吗？ //todo
                         if w_cid == v_cid:
                             #print("1==>", v_cid, w_cid, tot, k_v)
-                            tot -= k_v
+                            continue;
+                            #tot -= k_v
+                        
                         # k_v_in是从节点i连接到C中的节点的链路的总和
                         k_v_in = sum([v for k, v in self._G[v_vid].items() if k in self._cid_vertices[w_cid]])
-                        delta_Q = k_v_in - k_v * tot / self._m  # 由于只需要知道delta_Q的正负，所以少乘了1/(2*self._m)
+                        delta_Q1 = k_v_in - k_v * tot / self._m  # 由于只需要知道delta_Q的正负，所以少乘了1/(2*self._m)
+                        # above is deltaQ(i->C)
+
+                        # below is deltaQ(D->i)
+                        k_v_in2=sum([v for k, v in self._G[v_vid].items() if k in self._cid_vertices[v_cid]])
+                        tot2 = sum(
+                            [ sum(self._G[k].values()) + self._vid_vertex[k]._kin  for k in self._cid_vertices[v_cid]] )
+                        delta_Q2= -k_v_in2 - k_v**2 + 2*k_v*tot2
+
+                        # add the 2 deltas.
+                        delta_Q=delta_Q1+delta_Q2
                         # print("==>", v_cid, w_cid, delta_Q)
                         cid_Q[w_cid] = delta_Q
                         #print("2====>", v_cid, w_cid, "cid_Q=",cid_Q)
 
+                # 如果没有可以调整的社区编号，则跳出循环。
+                if len(cid_Q)==0:
+                    print("====> len(cid_Q):0")
+                    can_stop = True #只有有一个点的社区号调整过，这里就是F，就可以下一轮循环。
+                    mod_inc = False
+                    break;
+                    
                 # 取得最大增益的编号
                 cid, max_delta_Q = sorted(cid_Q.items(), key=lambda item: item[1], reverse=True)[0]
+                if log_max_deltaQ<= max_delta_Q:
+                    log_max_deltaQ = max_delta_Q
+                else:
+                    pass
+                    #print(max_delta_Q)
+
 
                 #需要好几个循环才能调整到 deltaQ<=0
                 #print("\t3====>","v_vid=",v_vid,  "v_cid=",v_cid, "cid=", cid, "max_delta_Q=",max_delta_Q) 
@@ -128,9 +170,14 @@ class Louvain:
                     can_stop = False #只有有一个点的社区号调整过，这里就是F，就可以下一轮循环。
                     
                     mod_inc = True
+
+            if iter_times_phaseI >= MAX_PHASE_I: #如果大型网络不收敛，则超过规定次数自动停止
+                can_stop=True;
             if can_stop: #while 死循环什么时候结束？对每个点遍历，没有可以调整标签的点时，结束 PhaseI.
                 #print("-- break PhaseI -- | mod_inc:",mod_inc)
                 break
+            print(f"\tmax_dleta_Q:{log_max_deltaQ}")
+
         #print("status after phaseI:")
         #print("\t {cid: vid arr}::", [item for item in self._cid_vertices.items() if len(item[1])>0])
         #print("\t {vid: vertex object}::", self._vid_vertex)
@@ -139,7 +186,7 @@ class Louvain:
 
 
 
-    # 网络凝聚阶段: 每个社区 合并为 超级节点
+    # phase II 网络凝聚阶段: 每个社区 合并为 超级节点
     def second_stage(self):
         print("== Phase II begin ==")
         cid_vertices = {}
@@ -192,6 +239,8 @@ class Louvain:
         self._vid_vertex = vid_vertex
         self._G = G
 
+        print("=====> cluster:", len(self._G))
+
 
 
     # 获取社区构成，就是 cid_vertices 中的键值对们的值: {社区编号: [节点编号arr]}
@@ -221,7 +270,7 @@ class Louvain:
             if mod_inc:
                 self.second_stage()
             else: # 什么时候整个优化过程结束呢？ mod_inc=F时，就是整个 phaseI  没有调整一个社区标签。
-                print("-------Stop PhaseII-----")
+                print("-------Stop Phase II-----")
                 break
         return self.get_communities()
 
@@ -229,8 +278,8 @@ class Louvain:
 
 if __name__ == '__main__':    
     # G = load_graph('data/club.txt')
-    #input_file='data/OpenFlights.txt'
-    input_file='data/t1.txt'
+    input_file='data/OpenFlights.txt'
+    #input_file='data/t3.txt'
 
     # input SNN_sparse_pbmc3k
     input_file='backup/snn_df.txt'
@@ -249,6 +298,7 @@ if __name__ == '__main__':
         if count>10:
             break;
         print("Community", count, ":", communitie)
+    print("====>>>>Total clusters:", len(communities))
     
 
 
